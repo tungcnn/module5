7a9ac863b5e05c4541f028b2df1e6228ca39ec4a
tungcnn <sontung.cnn@gmail.com>
2021-04-28 14:46:10 +0700

    add promise

---
+++ Chap1/promise/main.ts
@@ -0,0 +1,18 @@
+let money = 10000;
+const buyACar = (car: any) => {
+    return new Promise((((resolve, reject) => {
+        setTimeout(() => {
+            if (money >= 10000) {
+                resolve("can buy " + car);
+            } else {
+                reject("Do not enough money");
+            }
+        }, 100);
+    })))
+}
+money = 199999;
+const promise = buyACar("Vinfast").then(value => {
+    console.log(value);
+}, error =>{
+    console.log(error);
+})

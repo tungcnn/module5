19a0f0bbd5c164eaaefd47a8dce51821a6cf102a
tungcnn <sontung.cnn@gmail.com>
2021-04-29 08:55:38 +0700

    add font size editor

---
+++ angular-change-font-size/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import {ReactiveFormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FontSizeEditorComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
